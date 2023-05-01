84d2c09d225d4bc733c5e21b489c27c1a2b6248c
kamilolejarz@onet.eu <kamilolejarz@onet.eu>
2023-05-01 22:26:25 +0200

    spradzanie mozliwych ruchow bicia

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import javafx.print.PrintQuality;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -5,14 +6,21 @@ public class App {
         // Warcaby angielskie (warcaby amerykańskie, czekersy)
 
         Board b = new Board();
+        b.testBoard();
+        System.out.println(b);
+        b.TakeMoves(5, 4, Player.Side.WHITE);
+
+        //b.normalProperMovesForFigure(1, 0, Player.Side.BLACK);
+
+        // b.getAllNormalMoves(Player.Side.WHITE);
 
         // System.out.println(b.toString());
 
-        Player player = new Player(Player.Side.BLACK, "Player one");
-        Move move = new Move(new BoardSquare(4, 3), new BoardSquare(4, 1));
+        // Player player = new Player(Player.Side.BLACK, "Player one");
+        // Move move = new Move(new BoardSquare(4, 3), new BoardSquare(4, 1));
 
-        System.out.println(move);
-        System.out.println(player.makeMove(move, b));
+        // System.out.println(move);
+        // System.out.println(player.makeMove(move, b));
 
     }
 }
--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,8 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-import javax.lang.model.util.SimpleElementVisitor6;
+import javafx.print.PrintQuality;
+import javafx.scene.text.FontPosture;
 
 public class Board {
 
@@ -10,7 +12,7 @@ public class Board {
     int numWhiteKings;
     int numBlackKings;
 
-    private enum Figures {
+    public enum Figures {
         WHITE, BLACK, BLACK_KING, WHITE_KING, EMPTY;
     }
 
@@ -119,6 +121,33 @@ public class Board {
         return true;
     }
 
+    public void testBoard() {
+
+        board = new Figures[8][8];
+        board[5][4] = Figures.WHITE;
+        board[4][3] = Figures.BLACK;
+        board[4][5] = Figures.BLACK;
+        fillEmptyOnBoard();
+    }
+
+    public ArrayList<Move> getAllNormalMoves(Player.Side side) {
+
+        ArrayList<Move> allNormalMoves = new ArrayList<Move>();
+        Figures figure;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                figure = getFigure(i, j);
+                if (figure != Figures.EMPTY && ((side == side.WHITE && figure != Figures.BLACK_KING)
+                        || (side == side.BLACK && figure != Figures.WHITE_KING))) {
+                    allNormalMoves.addAll(normalProperMovesForFigure(i, j, side));
+                }
+            }
+        }
+        for (int i = 0; i < allNormalMoves.size(); i++)
+            System.out.println(allNormalMoves.get(i));
+        return allNormalMoves;
+    }
+
     public ArrayList<Move> normalProperMovesForFigure(int row, int col, Player.Side side) {
 
         ArrayList<Move> normalProperMoves = new ArrayList<Move>();
@@ -126,18 +155,22 @@ public class Board {
         Figures figure = getFigure(row, col);
         BoardSquare starBoardSquare = new BoardSquare(row, col);
 
+        int endRow;
+        int colLeft;
+        int colRight;
+        // dwie możliwości ruchu
         if (side == Player.Side.WHITE) {
             if (figure == Figures.WHITE) {
 
-                int endRow = row - 1;
-                int colLeft = col - 1;
-                int colRight = col + 1;
+                endRow = row - 1;
+                colLeft = col - 1;
+                colRight = col + 1;
 
                 if (endRow >= 0) {
                     if (colLeft >= 0 && getFigure(endRow, colLeft) == Figures.EMPTY) {
                         normalProperMoves.add((new Move(starBoardSquare, new BoardSquare(endRow, colLeft))));
                     }
-                    if (colRight < 8) {
+                    if (colRight < 8 && getFigure(endRow, colRight) == Figures.EMPTY) {
                         normalProperMoves.add((new Move(starBoardSquare, new BoardSquare(endRow, colRight))));
                     }
                 }
@@ -145,14 +178,151 @@ public class Board {
         } else {
             if (figure == Figures.BLACK) {
 
+                endRow = row + 1;
+                colLeft = col - 1;
+                colRight = col + 1;
+
+                if (endRow < 8) {
+                    if (colLeft >= 0 && getFigure(endRow, colLeft) == Figures.EMPTY) {
+                        normalProperMoves.add((new Move(starBoardSquare, new BoardSquare(endRow, colLeft))));
+                    }
+                    if (colRight < 8 && getFigure(endRow, colRight) == Figures.EMPTY) {
+                        normalProperMoves.add((new Move(starBoardSquare, new BoardSquare(endRow, colRight))));
+                    }
+                }
+            }
+        }
+        // 4 możliwości ruchu
+        if (figure == Figures.BLACK_KING || figure == Figures.WHITE_KING) {
+
+            // do góry
+            endRow = row - 1;
+            colLeft = col - 1;
+            colRight = col + 1;
+
+            if (endRow >= 0) {
+                if (colLeft >= 0 && getFigure(endRow, colLeft) == Figures.EMPTY) {
+                    normalProperMoves.add((new Move(starBoardSquare, new BoardSquare(endRow, colLeft))));
+                }
+                if (colRight < 8 && getFigure(endRow, colRight) == Figures.EMPTY) {
+                    normalProperMoves.add((new Move(starBoardSquare, new BoardSquare(endRow, colRight))));
+                }
+            }
+            // do dołu
+            endRow = row + 1;
+
+            if (endRow < 8) {
+                if (colLeft >= 0 && getFigure(endRow, colLeft) == Figures.EMPTY) {
+                    normalProperMoves.add((new Move(starBoardSquare, new BoardSquare(endRow, colLeft))));
+                }
+                if (colRight < 8 && getFigure(endRow, colRight) == Figures.EMPTY) {
+                    normalProperMoves.add((new Move(starBoardSquare, new BoardSquare(endRow, colRight))));
+                }
+            }
+        }
+        // Wypisanie możliwych ruchów
+        // for (int i = 0; i < normalProperMoves.size(); i++) {
+        // System.out.println(normalProperMoves.get(i));
+        // }
+        return normalProperMoves;
+    }
+
+    //ruchy bicia - obowiązkowe
+    public ArrayList<Move> TakeMoves(int row, int col, Player.Side side) {
+
+        ArrayList<Move> takeMoves = new ArrayList<Move>();
+        Figures figure = getFigure(row, col);
+        BoardSquare starBoardSquare = new BoardSquare(row, col);
+
+        int endRow;
+        int colLeft;
+        int colRight;
+
+        int enemyRow;
+        int enemyColLeft;
+        int enemyColRight;
+
+        if (side == Player.Side.WHITE) {
+            if (figure == Figures.WHITE) {
+
+                endRow = row - 2;
+                colLeft = col - 2;
+                colRight = col + 2;
+
+                enemyRow = row - 1;
+                enemyColLeft = col - 1;
+                enemyColRight = col + 1;
+
+                if (endRow >= 0 && enemyRow >= 0) {
+                    if (enemyColLeft >= 0 && (getFigure(enemyRow, enemyColLeft) == Figures.BLACK
+                            || getFigure(enemyRow, enemyColLeft) == Figures.BLACK_KING) && getFigure(endRow,colLeft)==Figures.EMPTY) {
+                                takeMoves.add(new Move(starBoardSquare, new BoardSquare(endRow, colLeft)));
+                    }
+                    if (enemyColRight < 8  && (getFigure(enemyRow, enemyColRight) == Figures.BLACK
+                    || getFigure(enemyRow, enemyColRight) == Figures.BLACK_KING) && getFigure(endRow,colLeft)==Figures.EMPTY){
+                        takeMoves.add(new Move(starBoardSquare, new BoardSquare(endRow, colRight)));
+                    }
+                }
+            }
+        } else {
+            endRow = row + 2;
+            colLeft = col - 2;
+            colRight = col + 2;
+
+            enemyRow = row + 1;
+            enemyColLeft = col - 1;
+            enemyColRight = col + 1;
+            if (endRow < 8 && enemyRow < 8) {
+                if (enemyColLeft >= 0 && (getFigure(enemyRow, enemyColLeft) == Figures.WHITE
+                        || getFigure(enemyRow, enemyColLeft) == Figures.WHITE_KING) && getFigure(endRow,colLeft)==Figures.EMPTY) {
+                            takeMoves.add(new Move(starBoardSquare, new BoardSquare(endRow, colLeft)));
+                }
+                if (enemyColRight < 8 && (getFigure(enemyRow, enemyColRight) == Figures.WHITE
+                || getFigure(enemyRow, enemyColRight) == Figures.WHITE_KING) && getFigure(endRow,colLeft)==Figures.EMPTY){
+                    takeMoves.add(new Move(starBoardSquare, new BoardSquare(endRow, colRight)));
+                }
             }
         }
 
-        if (figure == Figures.BLACK_KING || figure == Figures.WHITE_KING) {
+        if(figure==Figures.WHITE_KING || figure==Figures.BLACK_KING){
 
+            endRow = row - 2;
+            colLeft = col - 2;
+            colRight = col + 2;
+
+            enemyRow = row - 1;
+            enemyColLeft = col - 1;
+            enemyColRight = col + 1;
+
+            if (endRow >= 0 && enemyRow >= 0) {
+                if (enemyColLeft >= 0 && (getFigure(enemyRow, enemyColLeft) == Figures.BLACK
+                        || getFigure(enemyRow, enemyColLeft) == Figures.BLACK_KING) && getFigure(endRow,colLeft)==Figures.EMPTY) {
+                            takeMoves.add(new Move(starBoardSquare, new BoardSquare(endRow, colLeft)));
+                }
+                if (enemyColRight < 8 && (getFigure(enemyRow, enemyColRight) == Figures.BLACK
+                || getFigure(enemyRow, enemyColRight) == Figures.BLACK_KING) && getFigure(endRow,colLeft)==Figures.EMPTY){
+                    takeMoves.add(new Move(starBoardSquare, new BoardSquare(endRow, colRight)));
+                }
+            }
+
+            endRow = row + 2;
+            enemyRow = row + 1;
+
+            if (endRow < 8 && enemyRow < 8) {
+
+                if (enemyColLeft >= 0 && (getFigure(enemyRow, enemyColLeft) == Figures.WHITE
+                        || getFigure(enemyRow, enemyColLeft) == Figures.WHITE_KING) && getFigure(endRow,colLeft)==Figures.EMPTY) {
+                            takeMoves.add(new Move(starBoardSquare, new BoardSquare(endRow, colLeft)));
+                }
+                if (enemyColRight < 8 && (getFigure(enemyRow, enemyColRight) == Figures.WHITE
+                || getFigure(enemyRow, enemyColRight) == Figures.WHITE_KING) && getFigure(endRow,colLeft)==Figures.EMPTY){
+                    takeMoves.add(new Move(starBoardSquare, new BoardSquare(endRow, colRight)));
+                }
+            }
         }
-
-        return normalProperMoves;
+        // for(int i=0; i<takeMoves.size();i++)
+        //     System.out.println(takeMoves.get(i));
+        // return takeMoves;
     }
 
 }
